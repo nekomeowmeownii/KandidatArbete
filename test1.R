acc8b63c7c9eabd418665d5ad53e5c62f3521b72
kennyng@chalmers.se <182418361+nekomeowmeownii@users.noreply.github.com>
2026-05-13 13:48:30 +0200

    slaughter

--- test1.R
+++
@@ -1,9 +0,0 @@
-load("Z:\\Chalmers\\KANDIDAT\\programmering\\Monet.RData")
-monet_window <- owin(c(5, 1017), c(0, 767))
-subject1_data <- subset(Monet, id == 1 & inpic == 1)
-
-monet_ppp <- ppp(x = subject1_data$locX, 
-                 y = subject1_data$locY, 
-                 window = monet_window)
-
-plot(monet_ppp)
